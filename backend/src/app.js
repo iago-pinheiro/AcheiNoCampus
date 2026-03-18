b6b29c53e136e1807168f7772355ce1a8241bd38
Pedro Xavier <pedro_xavier9@outlook.com>
2026-03-18 14:44:19 -0300

    feat: implementar rotas da api

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,23 @@
 import express from "express";
+import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import itemRoutes from "./routes/itemRoutes.js";
+import homeRoutes from "./routes/homeRoutes.js";
+import sobreRoutes from "./routes/sobreRoutes.js";
 
 const app = express();
 
+// Middlewares
+app.use(cors());
 app.use(express.json());
 
+// Rotas da API
 app.use("/api/auth", authRoutes);
 app.use("/api/items", itemRoutes);
+app.use("/api/home", homeRoutes);
+app.use("/api/sobre", sobreRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Servidor do Achei no Campus rodando na porta ${PORT}`);
+  console.log(`Servidor do Achei no Campus rodando na porta ${PORT}`);
 });
